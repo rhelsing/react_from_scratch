4130ab2484f6e152870e24ea4c87bb5cb8c0b564
Ryan Helsing <ryanhelsing@gmail.com>
2017-07-22 21:06:05 -0400

    display vid

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyB3jhbripfHODc1Dj_JeCjHg8F7qfh_ATs'
 // component to produce HTML
@@ -11,18 +12,22 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
     YTSearch({key: API_KEY, term: 'ryan helsing'}, videos => {
-      this.setState({ videos: videos }) //could use {videos } because same key
+      this.setState({ videos: videos, selectedVideo: videos[0] }) //could use {videos } because same key
     })
   }
 
   render() {
     return (
       <div>
-      <SearchBar />
-      <VideoList videos={this.state.videos} />
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} />
       </div>
       )
   }
